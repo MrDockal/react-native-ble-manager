dc7584055d856afd0aa0b513c5caa7f19ca6a662
Jan Dočkal <dockal07@gmail.com>
2018-12-09 19:18:58 +0100

    Split advertising to iOS and Android

--- src/Model/Advertising.ts
+++ src/Model/Advertising.ts
@@ -1,9 +1,18 @@
 import { ManufacturerData } from "./ManufacturerData";
 
-export interface Advertising {
+export type Advertising = AdvertisingAndroid | AdvertisingIOS;
+
+export interface AdvertisingAndroid {
 	isConnectable: boolean;
 	localName: string;
 	manufacturerData: ManufacturerData;
 	serviceUUIDs: string[];
 	txPowerLevel: number;
+}
+
+export interface AdvertisingIOS {
+	kCBAdvDataChannel: number;
+	kCBAdvDataIsConnectable: number;
+	kCBAdvDataLocalName: string;
+	kCBAdvDataManufacturerData: ManufacturerData;
 }
